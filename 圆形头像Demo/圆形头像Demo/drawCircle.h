dc754efb656311efaf01ab37afc2dad78cbeabfc
邵瑞 <13983441921@139.com>
2016-01-28 16:40:16 +0800

    添加ZXing二维码扫描集成

---
+++ 圆形头像Demo/圆形头像Demo/drawCircle.h
@@ -0,0 +1,13 @@
+//
+//  drawCircle.h
+//  圆形头像Demo
+//
+//  Created by 邵瑞 on 16/1/28.
+//  Copyright © 2016年 wonders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface drawCircle : UIView
+
+@end
---
+++ 圆形头像Demo/圆形头像Demo/drawCircle.m
@@ -0,0 +1,21 @@
+//
+//  drawCircle.m
+//  圆形头像Demo
+//
+//  Created by 邵瑞 on 16/1/28.
+//  Copyright © 2016年 wonders. All rights reserved.
+//
+
+#import "drawCircle.h"
+
+@implementation drawCircle
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
