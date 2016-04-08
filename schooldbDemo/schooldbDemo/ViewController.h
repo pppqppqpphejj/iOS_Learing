d2945073dd8a65ac87d33be0e6e4f2b9bfe405a5
邵瑞 <13983441921@139.com>
2016-04-08 09:10:15 +0800

    读取全国所有学校数据db——demo

---
+++ schooldbDemo/schooldbDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  schooldbDemo
+//
+//  Created by 邵瑞 on 16/4/8.
+//  Copyright © 2016年 wondergroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ schooldbDemo/schooldbDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  schooldbDemo
+//
+//  Created by 邵瑞 on 16/4/8.
+//  Copyright © 2016年 wondergroup. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
